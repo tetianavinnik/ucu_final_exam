41acc205090db93b318c442764fcc203a9419460
tetianavinnik <92581380+tetianavinnik@users.noreply.github.com>
2021-12-28 10:46:31 +0200

    Update aliens.py

--- solutions/aliens.py
+++ solutions/aliens.py
@@ -1,2 +1,16 @@
-def get_systems_count(num: int) -> int:
-    pass # TODO
+import copy
+
+def calc_systems_count(num: int) -> int:
+    new_nums = []
+    count = 0
+    for i in range(2, num):
+        num_temp = copy.deepcopy(num)
+        new_num = ''
+        while num_temp>0:
+            new_num = str(num_temp % i) + new_num
+            num_temp //= i
+        if new_num == new_num[::-1]:
+            count += 1
+            print(new_num)
+        # new_nums.append(new_num)
+    return count
